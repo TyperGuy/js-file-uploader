066d598d2567ff5eb59d7d53ac4c181c2213f7e0
RJ Tony <ray.developper@gmail.com>
2021-07-29 22:51:28 +0100

    Pegando o nome do arquivo

--- controller.js
+++ controller.js
@@ -8,3 +8,12 @@ form.addEventListener("click",()=>{
     fileInput.click();
 
 });
+
+fileInput.onchange = ({target}) =>{
+
+  let file = target.files[0];
+  if(file){
+    let fileName = file.name;
+    console.log(fileName);
+  }
+}
