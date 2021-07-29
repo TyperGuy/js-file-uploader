25cd058ea70b4dee6ffaa7ce356d8f61e43a858a
RJ Tony <ray.developper@gmail.com>
2021-07-29 22:20:03 +0100

    Implementando o Input-file

---
+++ controller.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("form")
+, fileInput = form.querySelector(".file-input")
+, progressArea = document.querySelector(".progress-area")
+, uploadArea = document.querySelector(".uploaded-area");
+
+form.addEventListener("click",()=>{
+
+    fileInput.click();
+
+});
